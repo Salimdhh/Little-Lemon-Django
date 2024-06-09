0e5898f8c75b25e8d7fb42b0a754acaeafdafdb8
Salimdhh <108336215+Salimdhh@users.noreply.github.com>
2024-06-09 17:26:04 +0300

    Add files via upload

---
+++ littlelemon/restaurant/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from restaurant.models import Booking
+from restaurant.models import Menu
+# Register your models here.
+
+admin.site.register(Booking)
+admin.site.register(Menu)
